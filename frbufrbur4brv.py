5472de718602b42d289e5d3b38198374612a34f0
tyockely <tonyboycott146@gmail.com>
2026-09-25 13:29:20 +0000

    feffrfreerf

---
+++ frbufrbur4brv.py
@@ -0,0 +1,14 @@
+import time
+
+print("Hey its me, its verity!")
+print("Ask me anything?")
+
+choice = input('')
+
+print('You asked: ' + choice)
+time.sleep(1)
+print('hmmmmm......')
+time.sleep(3)
+print('I think I know the answer to that!')
+time.sleep(2)
+print('The answer is Paris!')
